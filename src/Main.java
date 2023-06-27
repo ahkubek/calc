e292bbcb0a7573a01c0190a5e3dcadaa8cb673b3
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-06-27 21:34:49 +0300

    проверка калькулятора

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,5 @@ public class Main {
         System.out.println("ответ " + res1);
         System.out.println("ответ " + res2);
         System.out.println("ответ " + res3);
-        System.out.println("проверка");
     }
 }
